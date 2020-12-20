332f5d2dd5b8ee564ee15c86280a62ee6277f799
Nick Conklin <nconkl1@outlook.com>
2020-12-19 21:21:23 -0500

    modified PlantCard to be more card-like

--- lib/plant_card.dart
+++ lib/plant_card.dart
@@ -1,27 +1,36 @@
 import 'package:flutter/material.dart';
 
 class PlantCard extends StatelessWidget {
-  final Color cardColor;
-  // final Widget cardChild;
-  final Function onPress;
   final String plantName;
+  final String plantNickname;
 
-  PlantCard({@required this.cardColor, @required this.plantName, this.onPress});
+  PlantCard({this.plantName, this.plantNickname});
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: onPress,
-      child: Container(
-        child: Row(
-          children: [
-            Text(plantName),
-          ],
+    return Container(
+      margin: EdgeInsets.fromLTRB(8, 16, 8, 0),
+      child: Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(8.0)),
         ),
-        margin: EdgeInsets.all(15.0),
-        decoration: BoxDecoration(
-          color: cardColor,
-          borderRadius: BorderRadius.circular(10.0),
+        child: ListTile(
+          leading: Icon(Icons.check),
+          title: Text(
+            plantName,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 20,
+            ),
+          ),
+          subtitle: Text(
+            plantNickname,
+            style: TextStyle(
+              fontStyle: FontStyle.italic,
+            ),
+          ),
+          // TODO: Get border radius to work with the tileColor
+          tileColor: Color(0xFF212121),
         ),
       ),
     );
